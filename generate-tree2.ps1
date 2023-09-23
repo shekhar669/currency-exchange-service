7ee8064182e8b358658eb91c7c02cd4327992124
shekhar669 <68266969+shekhar669@users.noreply.github.com>
2023-09-24 00:08:12 +0530

    Update generate-tree2.ps1

--- generate-tree2.ps1
+++ generate-tree2.ps1
@@ -1,6 +1,6 @@
 # Define the network drive and path
-$networkDrive = "X:"
-$networkPath = "\\server\share"
+$networkDrive = "Z:"
+$networkPath = "\\wn000106946\\IMPACTPRO_BUILDS"
 
 # Map the network drive
 New-PSDrive -Name $networkDrive -PSProvider FileSystem -Root $networkPath -Persist -Scope Global
